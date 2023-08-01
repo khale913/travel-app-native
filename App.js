1f7b1e6b9b2b4b24b322af80906fe971f9f95933
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-08-01 10:05:26 -0700

    init commit - basic nav

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
+import { View, Text, StyleSheet } from "react-native";
+import { Home, Profile, Liked, Details } from "./components";
+import colors from "./assets/colors/colors";
+import Entypo from "react-native-vector-icons/Entypo";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createBottomTabNavigator,
+  BottomTabBar,
+} from "@react-navigation/bottom-tabs";
+
+Entypo.loadFont();
+MaterialCommunityIcons.loadFont();
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      // style bottom tab bar to have a border radius of 10
+      screenOptions={{
+        tabBarStyle: styles.tabBar,
+        tabBarActiveTintColor: colors.orange,
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={32} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Liked"
+        component={Liked}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Entypo name="heart" size={32} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" size={32} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
-}
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  tabBar: {
+    backgroundColor: colors.white,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
 });
+
+export default App;
---
+++ components/index.js
@@ -0,0 +1,6 @@
+import Home from './Home';
+import Liked from './Liked';
+import Profile from './Profile';
+import Details from './Details';
+
+export {Home, Liked, Profile, Details};
